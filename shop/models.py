bdd286dc31f2df29f59da29a7e6c9e1c9b38b963
Raghavendra-coder <72242800+Raghavendra-coder@users.noreply.github.com>
2021-06-24 14:42:37 +0530

    models

--- shop/models.py
+++ shop/models.py
@@ -33,7 +33,7 @@ class Contact(models.Model):
     contact_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, default="")
     email = models.CharField(max_length=50, default="")
-    mobile = models.IntegerField(default="")
+    mobile = models.CharField(default="")
     desc = models.CharField(max_length=500, default="")
 
     def __str__(self):
